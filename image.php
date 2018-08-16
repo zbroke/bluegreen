b717391d467c79a8ca11a5294c0e79611cb50bb0
zbroke <patrick_sbrocca@yahoo.com>
2018-08-16 14:11:52 -0500

    Update image.php

--- image.php
+++ image.php
@@ -6,7 +6,7 @@ $white = imagecolorallocate($im, 255, 255, 255);
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  $color = imagecolorallocate($im, 255, 128, 128);
 } elseif ($deployment == 'green')  {
   $color = imagecolorallocate($im, 0, 255, 0);
 } else {
